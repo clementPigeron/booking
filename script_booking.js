1d464e4bd2ee2943abf137a610c99d708dce3d73
clementPigeron <clement.pigeron@gmail.com>
2025-05-12 18:33:59 +0200

    deux premiers filtres fonctionnent

--- script_booking.js
+++ script_booking.js
@@ -1,50 +1,76 @@
 const bfButton = document.getElementById("bfButton")
+const sheetButton = document.getElementById("sheetButton")
+
+const lodgeBox = document.querySelectorAll(".lodgeBox")
 const bfIndicator = document.querySelectorAll(".breakfast")
+const sheetIndicator = document.querySelectorAll(".sheet")
+
 let bfFilter = 0
+let sheetFilter = 0
 
 bfButton.addEventListener("click", () => {
-    if (bfFilter===0) {
-        bfFilter = 1
-    } else if (bfFilter===1) {
-        bfFilter = 2
-    } else {
-        bfFilter = 0
-    }
-
-    filterBf();
-    fcolorBf();
+    bfFilter = filterValueCycle(bfFilter);
+    buttonColorCycle(bfButton,bfFilter);
+    applyFilter();
 })
 
-function filterBf() {
-    if (bfFilter===0) {
-        bfIndicator.forEach((p) => {
-            p.parentNode.style.display = "block"
-        })
-    } else if (bfFilter===1) {
-        bfIndicator.forEach((p) => {
-            if (p.textContent.includes("✓")) {
-                p.parentNode.style.display = "block"
-            } else {
-                p.parentNode.style.display = "none"
-            }
-        })
+sheetButton.addEventListener("click", () => {
+    sheetFilter = filterValueCycle(sheetFilter);
+    buttonColorCycle(sheetButton,sheetFilter);
+    applyFilter();
+})
+
+function buttonColorCycle(button,filter) {
+    if (filter===0) {
+        button.style.backgroundColor = "white";
+    } else if (filter===1) {
+        button.style.backgroundColor = "green";
     } else {
-        bfIndicator.forEach((p) => {
-            if (p.textContent.includes("✓")) {
-                p.parentNode.style.display = "none"
-            } else {
-                p.parentNode.style.display = "block"
-            }
-        })
+        button.style.backgroundColor = "red";
     }
 }
 
-function fcolorBf() {
-    if (bfFilter===0) {
-        bfButton.style.backgroundColor = "white";
-    } else if (bfFilter===1) {
-        bfButton.style.backgroundColor = "green";
+function filterValueCycle(value) {
+    if (value===0) {
+        return 1
+    } else if (value===1) {
+        return 2
     } else {
-        bfButton.style.backgroundColor = "red";
+        return 0
+    }
+}
+
+function applyFilter() {
+    
+    lodgeBox.forEach((box) => {
+            box.style.display = "block"
+    })
+
+    if (bfFilter===1) {
+        bfIndicator.forEach((p) => {
+            if (p.textContent.includes("✕")) {
+               p.parentNode.style.display = "none"
+            }
+        })
+    } else if(bfFilter===2) {
+        bfIndicator.forEach((p) => {
+            if (p.textContent.includes("✓")) {
+               p.parentNode.style.display = "none"
+            }
+        })
+    }
+
+    if (sheetFilter===1) {
+        sheetIndicator.forEach((p) => {
+            if (p.textContent.includes("✕")) {
+               p.parentNode.style.display = "none"
+            }
+        })
+    } else if(sheetFilter===2) {
+        sheetIndicator.forEach((p) => {
+            if (p.textContent.includes("✓")) {
+               p.parentNode.style.display = "none"
+            }
+        })
     }
 }
